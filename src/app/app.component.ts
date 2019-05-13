3afafc1150111bbcc037712034f10a4814672bef
Yeison Betancourt Solis <y.betancourt@adateceu.com>
2019-05-13 17:56:31 -0500

    Create project admin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,109 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { MatPaginator, MatSort, MatTableDataSource } from "@angular/material";
+
+export interface ProjectData {
+  id: string;
+  project: string;
+  description: string;
+  url: string;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angular-admin';
+  displayedColumns: string[] = ["id", "project", "description", "url"];
+  dataSource: MatTableDataSource<ProjectData>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor() {
+    // Create projects
+    const PROJECTS = [
+      {
+        id: "1",
+        project: "Spoti",
+        description: "Aplicación de música",
+        url: "https://betancourtYeison.github.io/angular-spoti/"
+      },
+      {
+        id: "2",
+        project: "Movies",
+        description: "Aplicación de películas",
+        url: "https://betancourtyeison.github.io/angular-movies/"
+      },
+      {
+        id: "3",
+        project: "Youtube",
+        description: "Aplicación de youtube",
+        url: "https://betancourtyeison.github.io/angular-youtube/"
+      },
+      {
+        id: "4",
+        project: "Heroes",
+        description: "Aplicación de Heroes",
+        url: "https://betancourtyeison.github.io/angular-heroes/"
+      },
+      {
+        id: "5",
+        project: "Maps",
+        description: "Aplicación de Google maps",
+        url: "https://betancourtYeison.github.io/angular-map/"
+      },
+      {
+        id: "6",
+        project: "Auth0",
+        description: "Aplicación de login federado con google",
+        url: "https://betancourtyeison.github.io/angular-auth0/"
+      },
+      {
+        id: "7",
+        project: "Virtual Scroll",
+        description: "Aplicación de virtual scroll en angular",
+        url: "https://betancourtYeison.github.io/angular-virtualscroll/"
+      },
+      {
+        id: "8",
+        project: "Forms",
+        description: "Aplicación de formularios",
+        url: "https://betancourtyeison.github.io/angular-forms/"
+      },
+      {
+        id: "9",
+        project: "Graphics",
+        description: "Aplicación de Gráficos dinámicos",
+        url: "https://betancourtyeison.github.io/angular-graphics/"
+      },
+      {
+        id: "10",
+        project: "Pipes",
+        description: "Aplicación de angular pipes",
+        url: "https://betancourtyeison.github.io/angular-pipes/"
+      },
+      {
+        id: "11",
+        project: "Miscelaneos",
+        description: "Aplicación de componentes angular",
+        url: "https://betancourtYeison.github.io/angular-miscelaneos/"
+      }
+    ];
+
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(PROJECTS);
+  }
+
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
